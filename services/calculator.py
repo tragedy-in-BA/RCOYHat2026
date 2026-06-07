f2c44ce224dc4158eb3f42891234272d3993ffb7
Santiago <sbrizuela@dc.uba.ar>
2026-06-07 03:25:57 -0300

    feat: clean up

--- services/calculator.py
+++
@@ -1,7 +0,0 @@
-ADVANCE_RATE = 0.90
-
-
-def calcular_adelanto(valor_mensual: float, meses_restantes: int) -> dict[str, float]:
-    flujo_total = valor_mensual * meses_restantes
-    adelanto_estimado = flujo_total * ADVANCE_RATE
-    return {"flujo_total": flujo_total, "adelanto_estimado": adelanto_estimado}
